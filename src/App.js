8607b0a7d279ac8198d3c67f8689c207492c7d91
Sirin Srikier <sirinsrikier@Sirins-MacBook-Pro.local>
2019-07-01 07:46:01 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navigation from './components/navbar';
+import Dashboard from './components/dashboard';
+import Pokemon from './components/pokemon/pokemon';
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+          </Switch>
+        </div>
+      </Router>
+
+    );
+  }
 }
 
 export default App;
